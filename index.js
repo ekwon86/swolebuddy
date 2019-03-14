8afd97d69d429db357ac5563dc6be9731ddab7cf
Eugene Kwon <ekwon86@gmail.com>
2019-03-14 07:24:55 -0700

    now able to store automatically to db

--- index.js
+++ index.js
@@ -10,62 +10,79 @@ const iPhone = devices["iPhone X"];
 const config = require("./config.json");
 const dateFormat = require("dateformat");
 
+const MongoClient = require("mongodb").MongoClient;
+const url = "mongodb://localhost:27017/";
+
 const app = express();
 
-cron.schedule(
-  "0 12 * * *",
-  () => {
-    (async () => {
-      const browser = await puppeteer.launch();
-      const page = await browser.newPage();
+// cron.schedule(
+// "0 12 * * *",
+// () => {
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
 
-      await page.setViewport({ width: 1280, height: 800 });
-      await page.emulate(iPhone);
-      await page.goto(
-        "https://www.thequadguy.com/features?bnc=1&rsn=noOb&fromProt=&lng="
-      );
-      await page.focus("#Sentry_email");
-      await page.keyboard.type(config.testing.email);
+  await page.setViewport({ width: 1280, height: 800 });
+  await page.emulate(iPhone);
+  await page.goto(
+    "https://www.thequadguy.com/features?bnc=1&rsn=noOb&fromProt=&lng="
+  );
+  await page.focus("#Sentry_email");
+  await page.keyboard.type(config.testing.email);
 
-      await page.focus("#Sentry_password");
-      await page.keyboard.type(config.testing.password);
+  await page.focus("#Sentry_password");
+  await page.keyboard.type(config.testing.password);
 
-      await page.click("#Sentry_button");
+  await page.click("#Sentry_button");
 
-      await page.waitForSelector("#folderNav");
+  await page.waitForSelector("#folderNav");
 
-      await page.goto("https://www.thequadguy.com/dailypump");
+  await page.goto("https://www.thequadguy.com/dailypump");
 
-      await page.waitForSelector("#content");
+  await page.waitForSelector("#content");
 
-      let date = dateFormat(new Date(), "mm-dd-yyyy");
+  let date = dateFormat(new Date(), "mm-dd-yyyy");
 
-      let bodyPart = await page.evaluate(() => {
-        return document.querySelector(".sqs-block-content > h1").textContent;
-      });
+  let bodyPart = await page.evaluate(() => {
+    return document.querySelector(".sqs-block-content > h1").textContent;
+  });
 
-      let formattedBodyPart = bodyPart.replace(/\//g, ", ");
+  let formattedBodyPart = bodyPart.replace(/\//g, ", ");
 
-      let workout = await page.evaluate(() => {
-        return document.querySelector(".sqs-block-content > p:nth-child(2)")
-          .innerHTML;
-      });
+  let workout = await page.evaluate(() => {
+    return document.querySelector(".sqs-block-content > p:nth-child(2)")
+      .innerHTML;
+  });
 
-      let imgPath =
-        "./screenshots/" + date + " - " + formattedBodyPart + ".png";
+  let imgPath = "./screenshots/" + date + " - " + formattedBodyPart + ".png";
 
-      const workoutContent = await page.$(".sqs-block-content > :nth-child(2)");
+  const workoutContent = await page.$(".sqs-block-content > :nth-child(2)");
 
-      await workoutContent.screenshot({ path: imgPath });
+  await workoutContent.screenshot({ path: imgPath });
 
-      await browser.close();
-    })();
-  },
-  {
-    scheduled: true,
-    timezone: "America/Los_Angeles"
-  }
-);
+  await MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    let dbo = db.db("swolebuddy");
+    let myObj = {
+      bodypart: bodyPart,
+      workout: workout,
+      date: date
+    };
+
+    dbo.collection("workouts").insertOne(myObj, function(err, res) {
+      if (err) throw err;
+      db.close();
+    });
+  });
+
+  await await browser.close();
+})();
+// },
+// {
+// scheduled: true,
+// timezone: "America/Los_Angeles"
+// }
+// );
 
 // Body Parser Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
